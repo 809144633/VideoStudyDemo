39cdd19fe19ba424bdc95847bac501b60bfed58c
37745 <ziju.wang@1hai.cn>
2021-02-01 18:02:21 +0800

    feat(视频学习): 初步记录

--- app/src/main/java/com/ehi/videostudydemo/MainActivity.java
+++ app/src/main/java/com/ehi/videostudydemo/MainActivity.java
@@ -1,14 +1,28 @@
 package com.ehi.videostudydemo;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button btnDrawPic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnDrawPic = findViewById(R.id.btn_draw_pic);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_draw_pic:
+
+                break;
+            default:
+        }
     }
 }
---
+++ app/src/main/java/com/ehi/videostudydemo/drawPic/DrawPicActivity.java
@@ -0,0 +1,16 @@
+package com.ehi.videostudydemo.drawPic;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.ehi.videostudydemo.R;
+
+public class DrawPicActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_draw_pic);
+    }
+}
